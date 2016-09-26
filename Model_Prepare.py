d9dea778129dce6070b8e2cbb0398d0cbbb098fc
lqfortytwo <lqfmickey@gmail.com>
2016-09-26 15:24:03 -0400

    Modified model file dir

--- Model_Prepare.py
+++ Model_Prepare.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     parser.add_argument('--targe_text', help = 'Location of target text', default = './target_text/toy_story.txt')
     args = parser.parse_args()
 
-    target_name = args.targe_text.split("/")[-1] # Get target text file name. eg. "speeches.txt"
+    target_name = args.targe_text.split("/")[-1].split(".")[0] # Get target text file name. eg. "speeches.txt"
 
     download_model()
     print("Loading Skip-Vector Model...")
@@ -62,9 +62,10 @@ if __name__ == '__main__':
     print("Generating dictionary for the target text...")
     X = load_text(args.targe_text)
     worddict, wordcount = vocab.build_dictionary(X)
-    vocab.save_dictionary(worddict, wordcount, './target_dict/%s.pkl'%target_name)
-    print("Done! Saved dictionary under ./target_dict/ as %s.pkl"%target_name)
-
+    #vocab.save_dictionary(worddict, wordcount, './target_dict/%s_dict.pkl'%target_name)
+    #print("Done! Saved dictionary under ./target_dict/ as %s_dict.pkl"%target_name)
+    vocab.save_dictionary(worddict, wordcount, './%s/%s_dict.pkl'%(target_name, target_name))
+    print("Done! Saved dictionary under ./%s/ as %s_dict.pkl"%(target_name, target_name))
     """
     Step 2: Generating style vector for the target text.
     """
@@ -74,8 +75,10 @@ if __name__ == '__main__':
     print len(X)
     skip_vector = skipthoughts.encode(skmodel, X)
     style_vector = skip_vector.mean(0) # 0 indicate that mean method is performed over multiple axes, see numpy.mean document
-    np.save('./target_style/%s_style.npy'%target_name, style_vector)
-    print("Done! Saved style under ./target_style/ as %s_style.npy"%target_name)
+    #np.save('./target_style/%s_style.npy'%target_name, style_vector)
+    #print("Done! Saved style under ./target_style/ as %s_style.npy"%target_name)
+    np.save('./%s/%s_style.npy'%(target_name, target_name), style_vector)
+    print("Done! Saved style under ./%s/ as %s_style.npy"%(target_name, target_name))
 
     """
     Step 3: Training model for the target text.
@@ -83,5 +86,7 @@ if __name__ == '__main__':
     print("Training model for the target text...")
 
     C = X
-    train.trainer(X, C, skmodel, './target_dict/%s.pkl'%target_name, './target_model/%s.npz'%target_name)
-    print("Done! Saved model under ./target_model as %s.npz and %s.npz.pkl"%(target_name, target_name))
+    #train.trainer(X, C, skmodel, './target_dict/%s.pkl'%target_name, './target_model/%s.npz'%target_name)
+    #print("Done! Saved model under ./target_model as %s.npz and %s.npz.pkl"%(target_name, target_name))
+    train.trainer(X, C, skmodel, './%s/%s.pkl'%(target_name, target_name), './%s/%s.npz'%(target_name, target_name))
+    print("Done! Saved model under ./%s as %s.npz and %s.npz.pkl"%(target_name, target_name, target_name))
